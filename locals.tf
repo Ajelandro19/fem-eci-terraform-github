bba412ea8793384df8864cd7b7e6cc976a6f4520
Alejandro_GM <ag@rebajatuscuentas.com>
2024-05-19 23:15:51 -0600

    feat: added aws repos

--- locals.tf
+++ locals.tf
@@ -14,5 +14,19 @@ locals{
         topics = ["terraform", "github", "fem"]
         visibility = "public"
     }
+    "fem-eci-terraform-aws-network" = {
+        description = "Automation for AWS network."
+        gitignore_template = "Terraform"
+        name = "fem-eci-terraform-aws-network"
+        topics = ["terraform", "aws", "fem"]
+        visibility = "public"
+    }
+    "fem-eci-terraform-aws-cluster" = {
+        description = "Automation for AWS cluster."
+        gitignore_template = "Terraform"
+        name = "fem-eci-terraform-aws-cluster"
+        topics = ["terraform", "aws", "fem"]
+        visibility = "public"
+    }
   }
 }
