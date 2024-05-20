ce10f064035fda4bee80ae51af3969fbdb93b8af
Restyled.io <commits@restyled.io>
2024-05-20 05:18:47 +0000

    Restyled by terraform

--- locals.tf
+++ locals.tf
@@ -1,32 +1,32 @@
-locals{
+locals {
   repos = {
     "fem-eci-terraform-tfe" = {
-        description = "Automation for Terraform Enterprise."
-        gitignore_template = "Terraform"
-        name = "fem-eci-terraform-tfe"
-        topics = ["terraform", "tfe", "fem"]
-        visibility = "public"
+      description        = "Automation for Terraform Enterprise."
+      gitignore_template = "Terraform"
+      name               = "fem-eci-terraform-tfe"
+      topics             = ["terraform", "tfe", "fem"]
+      visibility         = "public"
     }
     "fem-eci-terraform-github" = {
-        description = "Automation for GitHub."
-        gitignore_template = "Terraform"
-        name = "fem-eci-terraform-github"
-        topics = ["terraform", "github", "fem"]
-        visibility = "public"
+      description        = "Automation for GitHub."
+      gitignore_template = "Terraform"
+      name               = "fem-eci-terraform-github"
+      topics             = ["terraform", "github", "fem"]
+      visibility         = "public"
     }
     "fem-eci-terraform-aws-network" = {
-        description = "Automation for AWS network."
-        gitignore_template = "Terraform"
-        name = "fem-eci-terraform-aws-network"
-        topics = ["terraform", "aws", "fem"]
-        visibility = "public"
+      description        = "Automation for AWS network."
+      gitignore_template = "Terraform"
+      name               = "fem-eci-terraform-aws-network"
+      topics             = ["terraform", "aws", "fem"]
+      visibility         = "public"
     }
     "fem-eci-terraform-aws-cluster" = {
-        description = "Automation for AWS cluster."
-        gitignore_template = "Terraform"
-        name = "fem-eci-terraform-aws-cluster"
-        topics = ["terraform", "aws", "fem"]
-        visibility = "public"
+      description        = "Automation for AWS cluster."
+      gitignore_template = "Terraform"
+      name               = "fem-eci-terraform-aws-cluster"
+      topics             = ["terraform", "aws", "fem"]
+      visibility         = "public"
     }
   }
 }
